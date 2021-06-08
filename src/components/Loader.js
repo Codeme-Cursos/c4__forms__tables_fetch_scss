ae43ddf2366cf534e81da91ba7d7071a65c220ce
Sebastian <sacm1046@gmail.com>
2021-06-07 23:02:55 -0400

    loader added

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SVGLoading from './icons/SVGLoading';
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <SVGLoading size={150} color="rgb(83, 83, 253)" />
+        </div>
+    )
+}
+
+export default Loader
--- src/pages/tasks/Home.js
+++ src/pages/tasks/Home.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Loader from "../../components/Loader";
 import HomeForm from "./components/HomeForm";
 import HomeTable from "./components/HomeTable";
 const { REACT_APP_API } = process.env;
@@ -10,11 +11,16 @@ const Home = () => {
   /* estado que representa el id de la tarea seleccionada para editar */
   const [taskId, setTaskId] = useState(null);
 
+  /* estado que representa las tareas existentes */
+  const [isLoading, setIsLoading] = useState(false);
+
   const getTasks = async () => {
     try {
+      setIsLoading(true);
       const res = await fetch(`${REACT_APP_API}/tasks`);
       const data = await res.json();
       setTasks(data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -27,11 +33,23 @@ const Home = () => {
   return (
     <main className="main">
       <div className="main__form">
-        <HomeForm {...{ tasks, setTasks, taskId, setTaskId }} getTasks={getTasks} />
+        <HomeForm
+          {...{ tasks, setTasks, taskId, setTaskId }}
+          getTasks={getTasks}
+        />
       </div>
 
       <div className="main__table">
-        <HomeTable {...{ tasks, setTasks, taskId, setTaskId }} getTasks={getTasks} />
+        {
+            isLoading ? (
+                <Loader />
+            ) : (
+                <HomeTable
+                {...{ tasks, setTasks, taskId, setTaskId }}
+                getTasks={getTasks}
+              />
+            )
+        }
       </div>
     </main>
   );
